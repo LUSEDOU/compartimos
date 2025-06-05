3e09263698c23bc727cfee525e9cc2919c5ccfc8
LUSEDOU <lusedou@gmail.com>
2025-06-05 12:08:14 -0500

    flutter app

---
+++ compartimos_bank/android/app/src/main/kotlin/pe/compartimos/compartimos/bank/MainActivity.kt
@@ -0,0 +1,6 @@
+package pe.compartimos.compartimos_bank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ compartimos_bank/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:compartimos_bank/app/app.dart';
+import 'package:compartimos_bank/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
